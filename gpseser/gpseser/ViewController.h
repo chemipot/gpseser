73e71e9ad06ea704f870b4a97e31aea3575bf08f
Jose Fernandez <jose.fernandez@rovio.com>
2016-07-11 14:08:48 +0300

    Initial commit to clone, build and test from remote repository

---
+++ gpseser/gpseser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gpseser
+//
+//  Created by Jose Fernanzed on 11/07/16.
+//  Copyright © 2016 Jose Fernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
